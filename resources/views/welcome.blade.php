64e4aa0d93898207b0579e7d567f25aa2d3a31c4
jiro.kirameki <ec2-user@ip-172-31-37-27.us-east-2.compute.internal>
2018-07-06 01:56:04 +0000

    background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,42 @@
-@extends('layouts.app')
+!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<img src ='' >
+        <title>kedot</title>
 
-<h1>YUNOKIHARA</h1>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+        <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+    </head>
+    <body class = 'toppage'>
+        @yield('cover')
 
-@endsection
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-1 col-lg-10">
+                    
+                    <!-- ログイン後に同じページにリダイレクトして以下を読み込む -->
+                    @if(Auth::check())
+                        <h1>welcome to kedot</h1>
+
+                    <!--未ログイン、未サインアップのユーザーには以下を表示-->
+                    @else
+                        <div>
+                            <!--routeのregister,loginは　web.php内の'Auth:routes'に含まれる-->
+                            <a href="{{ route('register') }}", class="btn btn-default" >SignUp</a><br> 
+                            <a href="{{ route('login')  }}", class="btn btn-default">LogIn</a>
+                        </div>
+                    @endif
+               
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// make:authのコマンドで自動で作られた
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
