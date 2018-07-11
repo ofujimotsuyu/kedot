a288cd69876c63b8561ded2df33e1995ad22a79c
jiro.kirameki <ec2-user@ip-172-31-35-132.us-east-2.compute.internal>
2018-07-11 05:10:03 +0000

    tasseido

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -30,23 +30,70 @@
   
         <?php $users = \DB::table('user_group')->where('group_id', $group->id)->get() ?>  
 
-         
+        <?php $members=0 ?>
     @foreach($users as $user)
         <?php 
+        $members= $members+1;
+        
         $id = $user->user_id;
         $name = App\User::find($id); ?>
         
-        {{ $user->user_id }}
+        
+        <p><img src="{{ url($name->avatar_filename)}}" alt="avatar" /></p>
         <h3>{{ $name->name }}</h3>
         
         <?php $records3 = \DB::table('activities')->where('user_id', $user->user_id)->where('group_id', $group->id)->get() ?>
         
+        <?php $tassei=0 ?>
         @foreach($records3 as $record)
-            {{ $record->record }}
+        <?php
+            echo $record->record;
+            $tassei=$tassei + $record->record;
+        ?>
         @endforeach
+        <?php echo $tassei; ?>
         
         <br>
     @endforeach
+        <?php echo $members; ?>
+    
+    <table width="95%" align="center" border="1" rules="none" bordercolor="#000099" cellspacing="0">
+    <caption>達成度</caption>
+    
+    <?php
+    $maxlen = 0;
+    $max = 0;
+        foreach ($users as $key => $user) {
+            $id = $user->user_id;
+            $name = App\User::find($id); 
+            
+        $records3 = \DB::table('activities')->where('user_id', $user->user_id)->where('group_id', $group->id)->get();
+        
+        $tassei=0;
+        foreach($records3 as $record) {
+            $tassei=$tassei + $record->record;
+        }
+        
+        $data[$key] = array($name->name, $tassei);
+        }
+        for($i = 0 ; $i < count($data) ; $i++) {
+            if(strlen($data[$i][0]) > $maxlen) {        
+                $maxlen = strlen($data[$i][0]);
+            }
+            if($data[$i][1] > $max) {           
+                $max = $data[$i][1];
+            }
+        }
+        for($i = 0 ; $i < count($data) ; $i++) {    
+            print("<tr>");
+            printf("<td width=\"%d\" align=\"right\">%s</td>", $maxlen * 10, $data[$i][0]);
+            printf("<td><hr size=\"10\" color=\"#cc6633\" align=\"left\" width=\"%d%%\"></td>", $data[$i][1] / $max * 100);
+            printf("<td width=\"%d\">%d</td>", strlen($max) * 10, $data[$i][1]);
+            print("</tr>\n");
+        }
+    ?>
+    
+    </table>
 
           
     @include('buttons.join_button', ['group' => $group])
