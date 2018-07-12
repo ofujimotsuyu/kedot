df97a36d47cc366808bd489a907b57cfdae9ff03
jiro.kirameki <ec2-user@ip-172-31-8-110.us-east-2.compute.internal>
2018-07-12 01:51:23 +0000

    編集ページにカテゴリーを追加

--- resources/views/groups/edit.blade.php
+++ resources/views/groups/edit.blade.php
@@ -6,7 +6,13 @@
       <div class="form-group">
           {!! Form::label('goal', 'ゴール') !!}
           {!! Form::textarea('goal', old('goal'), ['class' => 'form-control', 'rows' => '1']) !!}
+          
+          {!! Form::label('category','カテゴリー') !!}
+          <br>
+          {!! Form::select('category', [''=>'選択してください','ダイエット'=>'ダイエット','トレーニング'=>'トレーニング','学習'=>'学習','生活'=>'生活','健康・美容'=>'健康・美容','趣味'=>'趣味','その他'=>'その他'], ['class' => 'form-control quarter-form', 'rows' => '1']) !!}
 
+          <br>
+          
           {!! Form::label('to_do', '具体的にやること', ['class' => 'control-label']) !!}
           {!! Form::textarea('to_do', old('to_do'), ['class' => 'form-control', 'rows' => '1']) !!}
 
