de2dc6fa40a974e172b8eff5e6bb42119c35f222
jiro.kirameki <ec2-user@ip-172-31-8-110.us-east-2.compute.internal>
2018-07-12 01:33:52 +0000

    検索フォームの長さを変えた

--- resources/views/groups/search.blade.php
+++ resources/views/groups/search.blade.php
@@ -4,11 +4,11 @@
 
     <!--検索フォーム-->
     <!--actionでルートを指定-->
-    <div class="form-group">
+    <div class="form-group search">
         <form class='form-inline'method="get" action="./search" >
             <!--name=でcontrollerに送る名前を決定-->
             <input class='form-control gigigi col-xs-8 col-md-8 col-lg-8' type="text" name="search" >
-            {!! Form::select('category', [''=>'選択してください','ダイエット'=>'ダイエット','トレーニング'=>'トレーニング','学習'=>'学習','生活'=>'生活','健康・美容'=>'健康・美容','趣味'=>'趣味','その他'=>'その他'], ['class' => 'form-control quarter-form', 'rows' => '1']) !!}
+            {!! Form::select('category', [''=>'選択してください','ダイエット'=>'ダイエット','トレーニング'=>'トレーニング','学習'=>'学習','生活'=>'生活','健康・美容'=>'健康・美容','趣味'=>'趣味','その他'=>'その他'], ['class' => 'form-control searchcategory', 'rows' => '1']) !!}
             <button  class='form-control input_button' type="submit">
                 <span class="glyphicon glyphicon-search bobobo "></span>
             </button>       
