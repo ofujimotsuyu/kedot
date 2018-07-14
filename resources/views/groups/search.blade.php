6d08e2173763f80f65657e6a5e4d77e17d0d63f3
jiro.kirameki <ec2-user@ip-172-31-35-132.us-east-2.compute.internal>
2018-07-14 13:39:59 +0000

    暇なざきお３

--- resources/views/groups/search.blade.php
+++ resources/views/groups/search.blade.php
@@ -1,48 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <!--検索フォーム-->
-    <!--actionでルートを指定-->
-    <div class="form-group search nomform">
-        <h2 class="search_result">グループを検索しよう！</h2><br>
-
-        <form class='form-inline'method="get" action="./search" >
-            <!--name=でcontrollerに送る名前を決定-->
-            <input class='form-control gigigi col-xs-8 col-md-8 col-lg-8 first-form' type="text" name="search" placeholder = "キーワードからグループを検索" >
-            {!! Form::select('category', [''=>'カテゴリー検索','ダイエット'=>'ダイエット','トレーニング'=>'トレーニング','学習'=>'学習','生活'=>'生活','健康・美容'=>'健康・美容','趣味'=>'趣味','その他'=>'その他'], '選択してください', ['class' => 'form-control first-form category-search', 'rows' => '1']) !!}
-            <button  class='form-control input_button' type="submit">
-                <span class="glyphicon glyphicon-search bobobo"></span>
-            </button>       
-            <input type="hidden" name="_token" value="{{csrf_token()}}">
-        </form>
-    </div>
-
-    <!--検索結果を表示-->
-    <div class='search_result'>
-    <h2>
-    @if($goal||$category)「@endif
-    {{$goal}}
-    <!--フリーワードとカテゴリー両方で検索してれば、を表示-->
-    @if($goal&&$category), @endif
-    {{ $category }}
-    @if($goal||$category)」の検索結果@endif
-    </h2>
-    <tr class="result">
-    <div class = "groups">
-        @foreach($groups as $group)
-        <div class = "each_group">
-            <td><a href="{{ route('groups.show', ['id' => $group->id]) }}"><img src="{{url($group->group_filename)}}" alt="avatar"/><p>{{ $group->goal }}</p></a>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-1 col-lg-10">
+            <!--検索フォーム-->
+            <!--actionでルートを指定-->
+            <div class="form-group search nomform">
+                <h2 class="search_result">グループを検索しよう！</h2><br>
+        
+                <form class='form-inline'method="get" action="./search" >
+                    <!--name=でcontrollerに送る名前を決定-->
+                    <input class='form-control gigigi col-xs-8 col-md-8 col-lg-8 first-form' type="text" name="search" placeholder = "キーワードからグループを検索" >
+                    {!! Form::select('category', [''=>'カテゴリー検索','ダイエット'=>'ダイエット','トレーニング'=>'トレーニング','学習'=>'学習','生活'=>'生活','健康・美容'=>'健康・美容','趣味'=>'趣味','その他'=>'その他'], '選択してください', ['class' => 'form-control first-form category-search', 'rows' => '1']) !!}
+                    <button  class='form-control input_button' type="submit">
+                        <span class="glyphicon glyphicon-search bobobo"></span>
+                    </button>       
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                </form>
+            </div>
+        
+            <!--検索結果を表示-->
+            <div class='search_result'>
+            <h2>
+            @if($goal||$category)「@endif
+            {{$goal}}
+            <!--フリーワードとカテゴリー両方で検索してれば、を表示-->
+            @if($goal&&$category), @endif
+            {{ $category }}
+            @if($goal||$category)」の検索結果@endif
+            </h2>
+            <tr class="result">
+            <div class = "groups">
+                @foreach($groups as $group)
+                <div class = "each_group">
+                    <td><a href="{{ route('groups.show', ['id' => $group->id]) }}"><img src="{{url($group->group_filename)}}" alt="avatar"/><p>{{ $group->goal }}</p></a>
+                </div>
+        
+                @endforeach
+        
+            </div>
+            </tr>
+            
+            
+            <div align="center">
+            <br>{!! $groups->appends(['goal'=>$goal])->render() !!}
+            </div>
         </div>
-
-        @endforeach
-
     </div>
-    </tr>
-    
-    
-    <div align="center">
-    <br>{!! $groups->appends(['goal'=>$goal])->render() !!}
-    </div>
+</div>
 
 @endsection
