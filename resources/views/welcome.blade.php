9da6f006a6ca817379239a86c9a093e8e99fed56
jiro.kirameki <ec2-user@ip-172-31-37-27.us-east-2.compute.internal>
2018-07-17 01:18:42 +0000

    ちょっとカラム指定したよ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,17 +19,20 @@
     @if(Auth::check())
     <body>
         @include('commons.navbar')
-
-       <?php $groups = App\Group::paginate(18); ?>
-        <div class = "groups">
-            @foreach($groups as $group)
-            <div class = "each_group">
-                <a href="{{ route('groups.show', ['id' => $group->id]) }}"><img src="{{url($group->group_filename)}}" alt="avatar"/><p>{{ $group->goal }}</p></a>
+        <div class="row">
+            <div class="col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-1 col-lg-10">
+                <?php $groups = App\Group::paginate(18); ?>
+                <div class = "groups">
+                    @foreach($groups as $group)
+                    <div class = "each_group">
+                        <a href="{{ route('groups.show', ['id' => $group->id]) }}"><img src="{{url($group->group_filename)}}" alt="avatar"/><p>{{ $group->goal }}</p></a>
+                    </div>
+                    @endforeach
+                </div>
+                <div align="center">
+                    <br>{!! $groups->render() !!}
+                </div>
             </div>
-            @endforeach
-        </div>
-        <div align="center">
-            <br>{!! $groups->render() !!}
         </div>
     </body>
     <footer>
