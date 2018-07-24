a09f2f2561b1e5ddfbbc3ecc4d63e87114bb0a17
jiro.kirameki <ec2-user@ip-172-31-37-27.us-east-2.compute.internal>
2018-07-24 03:38:28 +0000

    ボタンの名前かえたよ

--- resources/views/buttons/homeru_button.blade.php
+++ resources/views/buttons/homeru_button.blade.php
@@ -5,12 +5,12 @@
               $name = \DB::table('users')->where('id', $user_id)->value('name')?>
         <p>{{ $name.'さんに'.'「'.$homekotoba.'」と褒めました。' }}</p>
         {!! Form::open(['route' => ['user.unhomeru', $user_id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unhomeru', ['class' => "btn btn-danger btn-block"]) !!}
+            {!! Form::submit('取り下げ', ['class' => "btn btn-danger btn-block"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.homeru', $user_id]]) !!}
-            {!! Form::text('iine', null, ['class' => 'form-control full-form', 'rows' => '1','placeholder' => '褒めてあげて！']) !!}
-            {!! Form::submit('Homeru', ['class' => "btn btn-primary btn-block"]) !!}
+            {!! Form::text('iine', null, ['class' => 'form-control full-form', 'rows' => '1','placeholder' => '例）すごーい！']) !!}
+            {!! Form::submit('褒めちゃう', ['class' => "btn btn-primary btn-block"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
