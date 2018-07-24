2c18b02518bec9143f036472b3058c7b6dfdf69e
jiro.kirameki <ec2-user@ip-172-31-8-110.us-east-2.compute.internal>
2018-07-24 06:53:40 +0000

    名前変更

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -323,8 +323,8 @@ ul {
             <li><a href="{{ route('users.show' ,['id'=>Auth::user()->id]) }}">My page</a></li>
             <li><a href="{{ route('groups.create' ,['id'=>Auth::user()->id]) }}">グループ作成</a></li>
             <li><a href="{{ route('groups.search' ,['id'=>Auth::user()->id]) }}">検索</a></li>
-            <li><a href="{{ route('user.favoritings' ,['id'=>Auth::user()->id]) }}">気になる目標</a></li>
-            <li><a href="{{ route('user.tassei' ,['id'=>Auth::user()->id]) }}">自分の達成度</a></li>
+            <li><a href="{{ route('user.favoritings' ,['id'=>Auth::user()->id]) }}">お気に入り</a></li>
+            <li><a href="{{ route('user.tassei' ,['id'=>Auth::user()->id]) }}">達成リスト</a></li>
             <li><a href="{{ route('users.homeraretas', ['id'=>Auth::user()->id]) }}">ほめてくれたひと<span class="badge" style="background:red">{{ $count_homerares }}</span></a></li>
             <li><a href="{{ route('users.index') }}">USERS</a></li>
             <li><a href="{{ route('user.requests', ['id' => Auth::User()->id]) }}">申請確認</a></li>
